8c8fb95fde9d2286582e39364f64dd704c1a1f77
Sébastien Mandaba <seb.mandaba@gmail.com>
2018-08-25 14:53:24 +0200

    Recipe steps added

--- Recipe.py
+++ Recipe.py
@@ -6,13 +6,13 @@ class Recipe:
     def __init__(self, recipe_link):
         self.recipe_link = recipe_link
         self.recipe_html_content = requests.get(recipe_link).content
-        self.recipe_object = BeautifulSoup(self.recipe_html_content, "html.parser")
+        self.recipe_object = BeautifulSoup(self.recipe_html_content, 'html.parser')
 
     def get_recipe_name(self):
         return self.recipe_object.title.text.rsplit(':')[0][:-1]
 
     def get_nb_person(self):
-        return self.recipe_object.find("div", {"class": "recipe-ingredients__qt-counter"}).input['value']
+        return self.recipe_object.find('div', {'class': 'recipe-ingredients__qt-counter'}).input['value']
 
     def get_ingredients(self):
         ingredients = self.recipe_object.find_all('li', 'recipe-ingredients__list__item')
@@ -23,3 +23,10 @@ class Recipe:
             ingredients_list.append({'ingredient_name': ingredient_name,
                                      'ingredient_quantity': ingredient_quantity})
         return ingredients_list
+
+    def get_recipe_steps(self):
+        recipe_steps = []
+        steps = self.recipe_object.find_all('li', 'recipe-preparation__list__item')
+        for step in steps:
+            recipe_steps.append(step.text.split('\t\t\t')[1].split('\t\t')[0])
+        return recipe_steps
--- scraper.py
+++ scraper.py
@@ -17,12 +17,16 @@ def extract_recipes_data(recipes):
     for recipe_html in recipes:
         recipe_link = root_url + recipe_html['href']
         recipe = Recipe(recipe_link)
+        recipe.get_recipe_steps()
 
         print(recipe.get_recipe_name())
         print('pour {} personnes\n'.format(recipe.get_nb_person()))
         print('Ingredients:')
         for ingredient in recipe.get_ingredients():
             print(ingredient['ingredient_quantity'] + ingredient['ingredient_name'])
+        print('\n')
+        for step in recipe.get_recipe_steps():
+            print('\t- {}'.format(step))
         print('\n\n')
 
 
