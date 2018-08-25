761d596c585c78b8080bf86c3e7b0a8a65d07046
Sébastien Mandaba <seb.mandaba@gmail.com>
2018-08-25 14:34:02 +0200

    code cleaned + Recipe class added

---
+++ Recipe.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class Recipe:
+    def __init__(self, recipe_link):
+        self.recipe_link = recipe_link
+        self.recipe_html_content = requests.get(recipe_link).content
+        self.recipe_object = BeautifulSoup(self.recipe_html_content, "html.parser")
+
+    def get_recipe_name(self):
+        return self.recipe_object.title.text.rsplit(':')[0][:-1]
+
+    def get_nb_person(self):
+        return self.recipe_object.find("div", {"class": "recipe-ingredients__qt-counter"}).input['value']
+
+    def get_ingredients(self):
+        ingredients = self.recipe_object.find_all('li', 'recipe-ingredients__list__item')
+        ingredients_list = []
+        for ingredient in ingredients:
+            ingredient_quantity = ingredient.find('span').text
+            ingredient_name = ingredient.find('span', 'ingredient').text
+            ingredients_list.append({'ingredient_name': ingredient_name,
+                                     'ingredient_quantity': ingredient_quantity})
+        return ingredients_list
--- scraper.py
+++ scraper.py
@@ -1,30 +1,29 @@
-
 import requests
-import unicodedata
 from bs4 import BeautifulSoup
-import pdb
 
-# recherche = input('recherche: ')
-recherche = 'tomate'
+from Recipe import Recipe
+
+recherche = input('recherche: ')
 root_url = 'http://www.marmiton.org'
 url = 'http://www.marmiton.org/recettes/recherche.aspx?aqt=' + recherche
 
 html = requests.get(url)
-html = str(html.content)
-soup = BeautifulSoup(html, "html.parser")
-recipes = soup.find_all('a', 'recipe-card')
+html_content = str(html.content)
+soup = BeautifulSoup(html_content, "html.parser")
+all_recipes = soup.find_all('a', 'recipe-card')
 
-i = 0
-while i < len(recipes):
-    recipe_link = root_url + recipes[i]['href']
-    recipe_html = requests.get(recipe_link)
-    recipe_html = recipe_html.content
-    soup2 = BeautifulSoup(recipe_html, "html.parser")
-    recipe_name = soup2.title.text.rsplit(':')[0][:-1]
-    # personnes = soup2.div['recipe-ingredients__qt-counter']
-    nb_person = soup2.find("div", { "class" : "recipe-ingredients__qt-counter"}).input['value']
-    # ingredient_list = soup2.findall("li", { "class" : "recipe-ingredients__list__item"})
-    pdb.set_trace()
-    print(recipe_name)
-    print('pour {} personnes\n'.format(nb_person))
-    i+=1
+
+def extract_recipes_data(recipes):
+    for recipe_html in recipes:
+        recipe_link = root_url + recipe_html['href']
+        recipe = Recipe(recipe_link)
+
+        print(recipe.get_recipe_name())
+        print('pour {} personnes\n'.format(recipe.get_nb_person()))
+        print('Ingredients:')
+        for ingredient in recipe.get_ingredients():
+            print(ingredient['ingredient_quantity'] + ingredient['ingredient_name'])
+        print('\n\n')
+
+
+extract_recipes_data(all_recipes)
